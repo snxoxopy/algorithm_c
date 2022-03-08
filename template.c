774bc9090da74dbd645db05efb78e4325099e705
snxoxopy <snoxox.py@gmail.com>
2022-03-08 13:59:20 +0900

    Add a template in C

---
+++ template.c
@@ -0,0 +1,28 @@
+/*
+---
+title:  "[C] "
+excerpt: ""
+toc: true
+toc_sticky: true
+toc_label: "Contents"
+categories:
+  - algorithm
+tags:
+  - 
+---
+
+문제이해: 분
+구현: 분
+Debug: 분
+참고자료
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    /* write you code here */
+
+}
+
